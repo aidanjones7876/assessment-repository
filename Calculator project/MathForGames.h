9e719a23ddd5f2d07bb9a4d6c0051bfa10a3aa00
aidanjones7876 <aidanj2525@gmail.com>
2025-10-06 22:46:43 -0700

    added Math
    Put in the xp and the roll chance

--- Calculator project/MathForGames.h
+++ Calculator project/MathForGames.h
@@ -1,1 +1,55 @@
 #pragma once
+using namespace std;
+#include <string>
+#ifndef MATH_H
+#define MATH_H
+
+class player {
+public: 
+	int level;
+	long long currentXP;
+	long long xpToNextLevel;
+
+	//constructor
+	player() : level(1), currentXP(0), xpToNextLevel(100) {}
+
+	void gainXP(int num) {
+		currentXP += num;
+		cout << "You have gained " << num << " XP. Current XP: " << currentXP << endl;
+
+		//check level up
+		while (currentXP >= xpToNextLevel) {
+			levelUp();
+		}
+	}
+private:
+	void levelUp() {
+		level++;
+		currentXP -= xpToNextLevel; //deduct xp required for level
+		xpToNextLevel = calculateXPFornextLevel(level); // calculate new xp requirement
+		cout << "Level up! New Level: " << level << ". XP to next level: " << xpToNextLevel << endl;
+	}
+
+	long long calculateXPFornextLevel(int currentLevel) {
+		//example: XP required increases exponentially
+		return 100LL * currentLevel * (currentLevel / 2);
+	}
+};
+struct LootItem {
+	string name;
+	int weight;
+};
+
+class LootTable {
+private:
+	LootItem items[5];
+	int itemCount;
+	int totalWeight;
+
+public:
+	LootTable();
+	void addItem(string name, int weight);
+	string rollLoot();
+	void displayTable();
+};
+#endif
--- Calculator project/main.cpp
+++ Calculator project/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "functions.h"
 #include "Text_Adventure_functions.h"
+#include "MathForGames.h"
 
 using namespace std;
 
@@ -21,10 +22,31 @@ int main() {
 	first.replaceTest();
 	first.allTests();*/
 	
+	//==== TEXT ADVENTURE FUNCTIONS====
+	/*welcomeScreen();
+	dungeon();*/
+	//end of text adventure functions
+	
+	player player;
+	cout << "Starting level: " << player.level << ", XP: " << player.currentXP << ", XP to next level: " << player.xpToNextLevel << endl;
 
-	welcomeScreen();
-	dungeon();
+	player.gainXP(40);
+	player.gainXP(70); //triggers level up
+	player.gainXP(800); //trigger multiple level ups
 	
-	
-	
+
+	//loot table
+	LootTable garage;
+
+	garage.addItem("old civic", 80);
+	garage.addItem("350Z", 15);
+	garage.addItem("Lambo", 4);
+	garage.addItem("MClaren Senna", 1);
+
+	garage.displayTable();
+
+	cout << "==== Roll simulation ====" << endl;
+	for (int i = 0; i < 10; i++) {
+		cout << "Drop" << (i + 1) << ": " << garage.rollLoot() << endl;
+	}
 }
